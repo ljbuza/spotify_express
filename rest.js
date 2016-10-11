04614b7898f40015fde1df4e295ab8be4ffc3a34
ljbuza <ljbuza@gmail.com>
2016-10-10 22:24:10 -0500

    eslint fixes

--- rest.js
+++ rest.js
@@ -1,35 +1,34 @@
-"use strict";
+const restify = require('restify');
+const https = require('https');
 
-var restify = require('restify');
-var https = require('https');
-
-function respond(req, res, next) {
-   getArtist(req.params.name, function(result) { res.send(result);} ); 
-    next();
-}
-var getArtist = function (name, callback) {
-    var url = 'https://api.spotify.com/v1/search?q=' + encodeURIComponent(name) + '&type=artist&limit=10';
-    https.get(url, function(response) {
-        var result = '';
-        response.setEncoding("utf8");
-        response.on('data', function (datum) {
-            result += datum;
-        });
-        response.on('end', function() {
-            callback(result);
-        });
-        response.on('error', function(err) {
-            console.log('got an error here: ', err.message);
-        });
-    }).on('error', function(err) { 
-        console.log('got error here instead', err);
+const getArtist = function (name, callback) {
+  const url = `https://api.spotify.com/v1/search?q=${encodeURIComponent(name)}&type=artist&limit=10`;
+  https.get(url, (response) => {
+    let result = '';
+    response.setEncoding('utf8');
+    response.on('data', (datum) => {
+      result += datum;
     });
+    response.on('end', () => {
+      callback(result);
+    });
+    response.on('error', (err) => {
+      console.log('got an error here: ', err.message);
+    });
+  }).on('error', (err) => {
+    console.log('got error here instead', err);
+  });
 };
 
-var server = restify.createServer();
+function respond(req, res, next) {
+  getArtist(req.params.name, (result) => { res.send(result); });
+  next();
+}
+
+const server = restify.createServer();
 server.get('/artist/:name', respond);
 server.head('/artist/:name', respond);
 
-server.listen(8081, function() {
-    console.log('%s listening at %s', server.name, server.url);
+server.listen(8081, () => {
+  console.log('%s listening at %s', server.name, server.url);
 });
